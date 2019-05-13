e2056b10f1037e764a05ac543ffade03044e7c21
YarHovor <yaroslav.hovorukha@gmail.com>
2019-05-13 21:08:25 +0300

    21 - VichUploaderBundle (Картинки для товаров)

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -1,10 +1,19 @@
 <?php
+
 namespace App\Entity;
+
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich; // вводится в все сущности где будут картинки
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Entity\File as EmbeddedFile;
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ * @Vich\Uploadable()
  */
 class Product
 {
@@ -42,6 +51,33 @@ class Product
      * @ORM\OneToMany(targetEntity="App\Entity\OrderItem", mappedBy="product", orphanRemoval=true)
      */
     private $orderItems;
+
+    // свойство для картинок
+
+    /**
+     * @var File
+     * @Vich\UploadableField(mapping="products", fileNameProperty="imageName", originalName="imageOriginalName")
+     *
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imageName;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updateAt;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imageOriginalName;
+
+
+
     public function __construct()
     {
         $this->isTop = false;
@@ -149,6 +185,65 @@ class Product
                 $orderItem->setProduct(null);
             }
         }
+        return $this;
+    }
+
+    //
+
+    public function getImage(): ?File
+    {
+        return $this->image;
+    }
+
+
+    public function setImage(?File $image): Product
+    {
+        $this->image = $image;
+
+        if ($image !== null) {
+            $this->updateAt = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    //
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(?string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    //
+
+    public function getUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(?\DateTimeInterface $updateAt): self
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    public function getImageOriginalName(): ?string
+    {
+        return $this->imageOriginalName;
+    }
+
+    public function setImageOriginalName(?string $imageOriginalName): self
+    {
+        $this->imageOriginalName = $imageOriginalName;
+
         return $this;
     }
 }
